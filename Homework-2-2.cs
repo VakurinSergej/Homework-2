5d5ae6c4d6817052cc114170412c0bbddac872e3
VakurinSergej <120665026+VakurinSergej@users.noreply.github.com>
2023-01-12 19:34:29 +0300

    Homework-2
    Ьн Homework

---
+++ Homework-2-2.cs
@@ -0,0 +1,14 @@
+﻿//  Программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a > 100 && a < 1000)
+{
+int b = (a % 10);
+Console.Write("Третья цифра числа: ");
+Console.WriteLine(b);
+}
+if (a < 100)
+{
+Console.Write("Третьей цифры нет");
+}
